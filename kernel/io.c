7aea99898f7040fcad4cc76a4823009a56c9063e
ProXgram <53403886+ProXgram@users.noreply.github.com>
2025-11-22 14:42:55 -0500

    Move IO functions to io.h for performance
    This file is now empty as its implementation has been moved to io.h for performance reasons.

--- kernel/io.c
+++ kernel/io.c
@@ -1,13 +1,5 @@
-#include <stdint.h>
-
+/* 
+ * Implementation moved to io.h as static inline functions for performance.
+ * This file is kept empty to satisfy build systems expecting kernel/*.c 
+ */
 #include "io.h"
-
-void outb(uint16_t port, uint8_t value) {
-    __asm__ volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
-}
-
-uint8_t inb(uint16_t port) {
-    uint8_t value;
-    __asm__ volatile ("inb %1, %0" : "=a"(value) : "Nd"(port));
-    return value;
-}
