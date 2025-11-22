15e7ad7308a92ac9b12946b57bc533f90cbe2dd1
ProXgram <53403886+ProXgram@users.noreply.github.com>
2025-11-22 14:43:46 -0500

    Add interrupts_enable_irq function declaration
    Added function to enable specified IRQ on the PIC.

--- kernel/include/interrupts.h
+++ kernel/include/interrupts.h
@@ -1,6 +1,11 @@
 #ifndef INTERRUPTS_H
 #define INTERRUPTS_H
 
+#include <stdint.h>
+
 void interrupts_init(void);
+
+/* Unmasks the specified IRQ (0-15) on the PIC */
+void interrupts_enable_irq(uint8_t irq);
 
 #endif /* INTERRUPTS_H */
