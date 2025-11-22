43f9128c6e8bffe74a49591b354230263c955fdf
ProXgram <53403886+ProXgram@users.noreply.github.com>
2025-11-22 16:01:39 -0500

    Update io.h

--- kernel/include/io.h
+++ kernel/include/io.h
@@ -18,6 +18,14 @@ static inline uint8_t inb(uint16_t port) {
     return value;
 }
 
+static inline void insw(uint16_t port, void* addr, uint32_t count) {
+    __asm__ volatile ("rep insw" : "+D"(addr), "+c"(count) : "d"(port) : "memory");
+}
+
+static inline void outsw(uint16_t port, const void* addr, uint32_t count) {
+    __asm__ volatile ("rep outsw" : "+S"(addr), "+c"(count) : "d"(port) : "memory");
+}
+
 static inline void io_wait(void) {
     outb(0x80, 0);
 }
