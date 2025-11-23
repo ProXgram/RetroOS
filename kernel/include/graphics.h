11c6100ba49fdcbe883c70ad13869907f298ac3f
ProXgram <53403886+ProXgram@users.noreply.github.com>
2025-11-22 22:20:06 -0500

    Update graphics.h

--- kernel/include/graphics.h
+++ kernel/include/graphics.h
@@ -8,6 +8,10 @@ void graphics_put_pixel(int x, int y, uint32_t color);
 void graphics_fill_rect(int x, int y, int w, int h, uint32_t color);
 void graphics_draw_char(int x, int y, char c, uint32_t fg, uint32_t bg);
 
+// New scaled drawing functions for the banner
+void graphics_draw_char_scaled(int x, int y, char c, uint32_t fg, uint32_t bg, int scale);
+void graphics_draw_string_scaled(int x, int y, const char* str, uint32_t fg, uint32_t bg, int scale);
+
 uint32_t graphics_get_width(void);
 uint32_t graphics_get_height(void);
 
