a61e2e11bf29c8c633aec4ecd9e08fa9a3d2fcfa
ProXgram <53403886+ProXgram@users.noreply.github.com>
2025-11-23 19:01:57 -0500

    Update graphics.h

--- kernel/include/graphics.h
+++ kernel/include/graphics.h
@@ -2,6 +2,7 @@
 #define GRAPHICS_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 void graphics_init(void);
 void graphics_put_pixel(int x, int y, uint32_t color);
@@ -11,6 +12,11 @@ void graphics_draw_char(int x, int y, char c, uint32_t fg, uint32_t bg);
 // New scaled drawing functions for the banner
 void graphics_draw_char_scaled(int x, int y, char c, uint32_t fg, uint32_t bg, int scale);
 void graphics_draw_string_scaled(int x, int y, const char* str, uint32_t fg, uint32_t bg, int scale);
+
+// Double Buffering
+void graphics_enable_double_buffer(void);
+void graphics_disable_double_buffer(void);
+void graphics_swap_buffer(void);
 
 uint32_t graphics_get_width(void);
 uint32_t graphics_get_height(void);
