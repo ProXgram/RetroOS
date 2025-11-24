50eed2bf651c402ef2eb73db152ce3fce219cbc5
ProXgram <53403886+ProXgram@users.noreply.github.com>
2025-11-23 21:24:01 -0500

    Update syscall.c

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -3,6 +3,8 @@
 #include "scheduler.h"
 #include "syslog.h"
 #include "ata.h"
+#include "io.h"
+#include "mouse.h"
 
 // Definition of the register state pushed by isr_syscall
 struct syscall_regs {
@@ -35,30 +37,25 @@ static int sys_disk_read(uint32_t lba, uint8_t count, uint8_t* buffer) {
     return -1; // Error
 }
 
+static void sys_shutdown(void) {
+    syslog_write("Syscall: Shutdown requested");
+    outw(0x604, 0x2000); // QEMU Shutdown
+    outw(0xB004, 0x2000); // Bochs Shutdown
+    outw(0x4004, 0x3400); // VirtualBox Shutdown
+}
+
+static void sys_get_mouse(MouseState* user_struct) {
+    // In a real OS, validate pointer first!
+    if (user_struct) {
+        MouseState k_state = mouse_get_state();
+        *user_struct = k_state;
+    }
+}
+
 // --- Dispatcher ---
 
 void syscall_dispatcher(struct syscall_regs* regs) {
-    // RAX holds the syscall number (but it wasn't pushed in the block, it's separate)
-    // We need to retrieve RAX. However, interrupts don't save RAX automatically 
-    // unless we pushed it. 
-    // Let's look at entry.asm: we didn't push RAX.
-    // BUT, the C compiler might clobber RAX as a return value register.
-    // We need to access the RAX from the previous context.
-    
-    // Actually, in standard ABI, RAX is the return value. 
-    // We should probably modify entry.asm to push RAX or read it directly.
-    // For simplicity here, we will assume the user put the syscall number in RDI 
-    // (first arg) for this specific demo, or fix entry.asm. 
-    
-    // Let's assume standard convention: RAX is syscall number.
-    // We need to grab it from the frame.
-    // Since we didn't push it in `isr_syscall`, it's still in the CPU register 
-    // until we clobber it. But we are in a C function now.
-    
-    // FIX: We will rely on the fact that `isr_syscall` didn't push RAX, 
-    // so we can't easily get it unless we modify `isr_syscall`.
-    // For this implementation, let's assume syscall number is passed in RDI (Arg 1).
-    
+    // Syscall number passed in RDI (Argument 1)
     uint64_t syscall_num = regs->rdi; 
 
     switch (syscall_num) {
@@ -72,11 +69,14 @@ void syscall_dispatcher(struct syscall_regs* regs) {
             sys_log((const char*)regs->rsi);
             break;
         case 3: // Disk Read
-            // RDI=3, RSI=LBA, RDX=Count, RCX=Buffer
-            // Note: We pushed RCX, so it's in regs.
-            // However, the mapping of registers depends on the push order.
-            // regs->rsi is the 2nd argument from user perspective
             sys_disk_read((uint32_t)regs->rsi, (uint8_t)regs->rdx, (uint8_t*)regs->rcx);
+            break;
+        case 4: // Shutdown
+            sys_shutdown();
+            break;
+        case 5: // Get Mouse State
+            // RSI contains the pointer to the User struct
+            sys_get_mouse((MouseState*)regs->rsi);
             break;
         default:
             kprintf("Unknown Syscall: %d\n", (int)syscall_num);
