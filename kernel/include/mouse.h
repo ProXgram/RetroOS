94e92107947a9ecf060268d4f89aa9cafc56fd86
ProXgram <53403886+ProXgram@users.noreply.github.com>
2025-11-23 21:33:04 -0500

    Update mouse.h

--- kernel/include/mouse.h
+++ kernel/include/mouse.h
@@ -15,7 +15,7 @@ void mouse_init(void);
 void mouse_handle_interrupt(void);
 MouseState mouse_get_state(void);
 
-// New Sensitivity Controls
+// Sensitivity controls
 void mouse_set_sensitivity(int sense);
 int mouse_get_sensitivity(void);
 
