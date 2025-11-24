1df1d9e924a89b3ae0016cebd13c25a8518428d4
ProXgram <53403886+ProXgram@users.noreply.github.com>
2025-11-23 19:05:57 -0500

    Update gdt.h

--- kernel/include/gdt.h
+++ kernel/include/gdt.h
@@ -8,4 +8,7 @@ extern uint8_t* const g_kernel_stack_top;
 
 void gdt_init(void);
 
+// Used by the scheduler to update the RSP0 in TSS when switching tasks
+void gdt_set_kernel_stack(uint64_t stack_top);
+
 #endif /* GDT_H */
