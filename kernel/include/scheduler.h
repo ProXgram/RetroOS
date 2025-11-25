729b955ef744a6550b492af41e34b7aeafebbd04
ProXgram <53403886+ProXgram@users.noreply.github.com>
2025-11-24 22:01:12 -0500

    Update scheduler.h

--- kernel/include/scheduler.h
+++ kernel/include/scheduler.h
@@ -4,11 +4,17 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+typedef enum {
+    TASK_READY,
+    TASK_DEAD
+} TaskState;
+
 typedef struct {
     uint64_t id;
     uint64_t rsp;
     uint64_t kernel_stack_top; // For Ring 3 -> 0 transitions
     bool is_user;
+    TaskState state;
     void* next;
 } Task;
 
@@ -16,6 +22,7 @@ void scheduler_init(void);
 void spawn_task(void (*entry_point)(void));
 void spawn_user_task(void (*entry_point)(void));
 void schedule(void);
+void exit_current_task(void);
 
 // Assembly helper
 extern void context_switch(uint64_t* old_sp_ptr, uint64_t new_sp);
