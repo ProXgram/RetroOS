7a5bc084c9e7b2a993eb7135f27ef59cf58f9fc0
ProXgram <53403886+ProXgram@users.noreply.github.com>
2025-11-25 20:18:06 -0500

    Update graphics.h

--- kernel/include/graphics.h
+++ kernel/include/graphics.h
@@ -13,6 +13,9 @@ void graphics_draw_char(int x, int y, char c, uint32_t fg, uint32_t bg);
 void graphics_draw_char_scaled(int x, int y, char c, uint32_t fg, uint32_t bg, int scale);
 void graphics_draw_string_scaled(int x, int y, const char* str, uint32_t fg, uint32_t bg, int scale);
 
+// Advanced Drawing
+void graphics_fill_rect_alpha(int x, int y, int w, int h, uint32_t color, uint8_t alpha);
+
 // Double Buffering
 void graphics_enable_double_buffer(void);
 void graphics_disable_double_buffer(void);
